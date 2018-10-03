59c2e03476536429cdf79e734ceb82f2b54f92a7
Saint-Jean Germila <germila@noukod.com>
2018-10-03 16:01:55 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,15 +2,7 @@ class Cat
   attr_writer :name
   attr_reader :name
 
-  def initialize
-    name
-  end
-  def name=(cat_name)
-    @cat_name = new_name
-  end
-  def name
-    puts "meow!"
-  end
+
   end
 
 maru = Cat.new
